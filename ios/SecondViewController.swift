0ff6d3156a306ddfbcbae5bb6db87b2ba383e94e
armin langhofer <armin@langhofer.net>
2017-04-09 18:45:54 +0200

    second: providing list of hosts

--- ios/SecondViewController.swift
+++ ios/SecondViewController.swift
@@ -1,10 +1,37 @@
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let list = Hosts.sharedInstance.hosts as! [Dictionary<String, Any>]
+        print ("count: \(list.count)")
+        return list.count
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        
+        let list = Hosts.sharedInstance.hosts as! [Dictionary<String, Any>]
+        
+        print ("list entry")
+        print (list[indexPath.row]["host"])
+        cell.textLabel?.text = list[indexPath.row]["host"] as? String
+        return cell
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
