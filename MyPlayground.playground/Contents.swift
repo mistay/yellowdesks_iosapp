2ebd8199d0fd26ae4d1988413ac42b711009d005
armin langhofer <armin@langhofer.net>
2017-04-09 18:44:04 +0200

    added playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+var str = "Hello, playground"
+
+str
+
+var a = URL(string: "http://api.yellowdesks.com/hosts?format=json")
+
+
+var foo = URLRequest(url: a!)
+
+foo
+
