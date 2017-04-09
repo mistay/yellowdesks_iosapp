2acee5c08e1a0b288768cb6fe73e2e172082f72c
armin langhofer <armin@langhofer.net>
2017-04-09 18:45:44 +0200

    firstview: loading pins after map completed loading OR after hosts compleated loading

--- ios/FirstViewController.swift
+++ ios/FirstViewController.swift
@@ -5,45 +5,26 @@ import Foundation
 
 
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MKMapViewDelegate {
 
     //@IBOutlet var Map: MKMapView!
     
     @IBOutlet var map: MKMapView!
+    
+    
+    let urlPrefix = String("https://api.yellowdesks.com")!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        let location = CLLocationCoordinate2DMake(
-            47.809490,
-            13.055010
-        )
-        
-        let annotation = ColorPointAnnotation(pinColor: UIColor.yellow)
-        
-        annotation.coordinate = location
-        annotation.title = "bla"
-        annotation.subtitle = "subbla"
-        
-        map.addAnnotation(annotation)
-        
-        
-        
-        
-        //let requestURL: NSURL = NSURL(string: "https://api.yellowdesks.com/hosts?format=json")!
-        
-        let requestURL = URL( string: "https://api.yellowdesks.com/hosts?format=json")!
-        
-        
+        let requestURL = URL( string: self.urlPrefix + "/hosts?format=json")!
         let session = URLSession.shared
         
-        
-        
-        //let task = session.dataTask (with: urlRequest as URLRequest) {
-        
-        print("starting")
+        print("downloading hosts ...")
         let task = session.dataTask(with: requestURL) {
             (data, response, error) -> Void in
         
@@ -53,15 +34,80 @@ class FirstViewController: UIViewController {
             
             if (statusCode == 200) {
                 print("Everyone is fine, file downloaded successfully.")
-                print ("mime: " + (httpResponse.mimeType)!)
-    
-                let dataString = String(data: data!, encoding: String.Encoding.utf8)
+                Hosts.sharedInstance.hosts = try? JSONSerialization.jsonObject(with: data!, options: [])
+                //print ("json: \(self.hosts)")
+                self.hostsLoaded()
                 
-                print ("data: \(dataString)")
             }
         }
         
         task.resume()
+    }
+    
+    var annotationsLoaded : Bool = false
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        print("mapViewDidFinishLoadingMap start")
+        addAnnotations()
+        print("mapViewDidFinishLoadingMap done")
+    }
+    func addAnnotations() {
+        if Hosts.sharedInstance.hosts == nil {
+            return;
+        }
+        
+        let list = Hosts.sharedInstance.hosts as! [Dictionary<String, Any>]
+        print ("hosts: \(list.count)")
+        
+        if (!annotationsLoaded) {
+            annotationsLoaded = true
+
+            
+            print("male hosts...")
+            for host in Hosts.sharedInstance.hosts as! [Dictionary<String, Any>] {
+                
+                
+                let lat = host["lat"] as? Double
+                let lng = host["lng"] as? Double
+                let nickname = host["host"] as? String
+                let title = host["title"] as? String
+                
+                print ("title: \(title)")
+                if lat != nil && lng != nil && nickname != nil {
+                    let location = CLLocationCoordinate2DMake(
+                        lat!,
+                        lng!
+                    )
+                    print ("location: \(location)")
+                    
+                    let annotation = ColorPointAnnotation(pinColor: UIColor.yellow)
+                    
+                    print(annotation)
+                    annotation.coordinate = location
+                    annotation.title = nickname
+                    annotation.subtitle = title
+                    
+                    
+                    map.addAnnotation(annotation)
+                    
+                    
+                    print("host annotation hinzugefügt!")
+
+                    
+                    //print("lat: \(lat) lng: \(lng)")
+                    
+                    
+                    
+                }
+            }
+            print("fertig mit malen...")
+        }
+    }
+    
+    func hostsLoaded() {
+        print("hostsLoaded() begin")
+        addAnnotations()
+        print("hostsLoaded() fertig")
     }
 
     override func didReceiveMemoryWarning() {
